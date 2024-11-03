c6877f825ffa31650dd45855bccdd394a7d07031
Grzegorz <153862859+GK1203@users.noreply.github.com>
2024-11-03 19:19:22 +0100

    Update select.cy.js

--- cypress/e2e/select.cy.js
+++ cypress/e2e/select.cy.js
@@ -3,23 +3,18 @@
 describe("Wybór konkretnego produktu na stronie sklepu", () => {
     it("Wejście na stronę", () => {
         cy.visit("http://www.automationpractice.pl/index.php");
-        cy.wait(1000);
     })
     it("Wybór zakładki odzieży", () => {
         cy.get("#block_top_menu > ul > li:nth-child(1) > a").click();
-        cy.wait(1000);
     })
     it("Definiowanie rozmiaru, koloru itd", () => {
         cy.get("#layered_category_4").check().should("be.checked");
-        cy.wait(1000);
         cy.get("#layered_id_attribute_group_3").check().should("be.checked");
-        cy.wait(1000);
     })
     it("Wejście w produkt", () => {
         cy.get("#center_column > ul > li.ajax_block_product.col-xs-12.col-sm-6.col-md-4.last-line.last-item-of-tablet-line.last-mobile-line > div > div.left-block").trigger("mouseover");
         cy.get("#center_column > ul > li.ajax_block_product.col-xs-12.col-sm-6.col-md-4.last-line.last-item-of-tablet-line.last-mobile-line > div > div.right-block > div.button-container > a").click();
         cy.get("#group_1").select("L");
-        cy.wait(1000);
         cy.get("#color_8").click();
         cy.get("#add_to_cart > button").click();
     })
@@ -27,7 +22,3 @@ describe("Wybór konkretnego produktu na stronie sklepu", () => {
         cy.contains("Product successfully added to your shopping cart");
     })
 })
-
-
-
-//#center_column > ul > li.ajax_block_product.col-xs-12.col-sm-6.col-md-4.last-line.last-item-of-tablet-line.last-mobile-line > div > div.left-block
