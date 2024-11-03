0641b3e12530241dcb8759efc235c5ec4989fd3f
Grzegorz <153862859+GK1203@users.noreply.github.com>
2024-11-03 19:20:44 +0100

    Update contact.cy.js

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -3,19 +3,16 @@
 describe("Wejście do zakładki Contact US oraz wysłanie wiadomości", () => {
     it("Otworzenie strony sklepu", () => {
         cy.visit("http://www.automationpractice.pl/index.php");
-        cy.wait(1000);
         cy.url().should("include", "http://www.automationpractice.pl/index.php");
     })
     it("Przejście do formularza kontaktowego", () => {
         cy.get("#contact-link > a").click();
-        cy.wait(1000);
         cy.url().should("include", "http://www.automationpractice.pl/index.php?controller=contact");
     })
     it("Uzupełnienie danych formularza oraz jego wysłanie", () => {
         cy.get("#id_contact").select("Webmaster");
         cy.get("#email").type("gk1122@gmail.com");
         cy.get("#id_order").type("Reklamacja");
-        cy.wait(1000);
         cy.get("#message").type("Składam reklamacje na obsługę sklepu");
         cy.get("#submitMessage").click();
     })
